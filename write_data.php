905a631063c622580070923dcf82f9570057f8dd
gorkemer <gorkemerr@gmail.com>
2020-04-01 12:26:31 +0300

    changed write code method

--- write_data.php
+++ write_data.php
@@ -1,51 +1,8 @@
 <?php
-
-// this path should point to your configuration file.
-include('database_config.php');
-
-$data_array = json_decode(file_get_contents('php://input'), true);
-
-try {
-  $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname", $username, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  // First stage is to get all column names from the table and store
-  // them in $col_names array.
-  $stmt = $conn->prepare("SHOW COLUMNS FROM `$table`");
-  $stmt->execute();
-  $col_names = array();
-  while($row = $stmt->fetchColumn()) {
-    $col_names[] = $row;
-  }
-  // Second stage is to create prepared SQL statement using the column
-  // names as a guide to what values might be in the JSON.
-  // If a value is missing from a particular trial, then NULL is inserted
-  $sql = "INSERT INTO $table VALUES(";
-  for($i = 0; $i < count($col_names); $i++){
-    $name = $col_names[$i];
-    $sql .= ":$name";
-    if($i != count($col_names)-1){
-      $sql .= ", ";
-    }
-  }
-  $sql .= ");";
-  $insertstmt = $conn->prepare($sql);
-  for($i=0; $i < count($data_array); $i++){
-    for($j = 0; $j < count($col_names); $j++){
-      $colname = $col_names[$j];
-      if(!isset($data_array[$i][$colname])){
-        $insertstmt->bindValue(":$colname", null, PDO::PARAM_NULL);
-      } else {
-        $insertstmt->bindValue(":$colname", $data_array[$i][$colname]);
-      }
-    }
-    $insertstmt->execute();
-  }
-  echo '{"success": true}';
-
-
-
-} catch(PDOException $e) {
-  echo '{"success": false, "message": ' . $e->getMessage();
-}
-$conn = null;
+$post_data = json_decode(file_get_contents('php://input'), true); 
+// the directory "data" must be writable by the server
+$name = "data/".$post_data['filename'].".csv"; 
+$data = $post_data['filedata'];
+// write the file to disk
+file_put_contents($name, $data);
 ?>

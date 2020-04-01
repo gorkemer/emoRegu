8f28c8f80c1c22af4b4b5cf672ab9db9f4eae1e4
gorkemer <gorkemerr@gmail.com>
2020-04-01 10:05:27 +0300

    added a doctype line to index

--- database_config.php
+++ database_config.php
@@ -1,5 +1,5 @@
 <?php
-  $servername = "197.198.77.203";
+  $servername = "197.198.77.203"; // local host or server ip address?
   $port = 3306;
   $username = "infecteduser";
   $password = "karabiber10"; //if it asks mysql user root, if not: nyhe86bp

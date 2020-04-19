ed00c0496ea0e3260aaa45240ec72a5752480619
gorkemer <gorkemerr@gmail.com>
2020-04-19 11:43:57 +0300

    deleted reaction time folder

--- ReactionTime/database_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $servername = "localhost"; // local host or server ip address?
-  $port = 3306;
-  $username = "root";
-  $password = "karabiber10"; //if it asks mysql user root, if not: nyhe86bp
-  $dbname = "ReactionTimeDatabase";
-  $table = "dataColumns";
-?>
